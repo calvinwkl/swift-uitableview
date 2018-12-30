5a76cf5446454300b7433df36188d62693083f52
Calvin Wong <kalokwong9@gmail.com>
2018-12-30 16:05:51 +0800

    Add UITableView project

---
+++ UITableView/UITableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableView
+//
+//  Created by Wong Ka Lok on 30/12/2018.
+//  Copyright © 2018 Wong Ka Lok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ UITableView/UITableViewTests/UITableViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  UITableViewTests.swift
+//  UITableViewTests
+//
+//  Created by Wong Ka Lok on 30/12/2018.
+//  Copyright © 2018 Wong Ka Lok. All rights reserved.
+//
+
+import XCTest
+@testable import UITableView
+
+class UITableViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
